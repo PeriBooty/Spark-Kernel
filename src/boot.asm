01487bd94fe2d0e079004e573e092d1ca7b6e6e2
VisualDevelopment <drherocrafterslab@gmail.com>
2019-10-27 14:42:27 +0200

    spark/kernel: boot.asm->fixed indentation

--- src/boot.asm
+++ src/boot.asm
@@ -28,70 +28,70 @@ mb_header:
     dd 0
     dd 0
     dd 0
-	dd 1
-	dd 1280
-	dd 720
-	dd 32
+    dd 1
+    dd 1280
+    dd 720
+    dd 32
 
 section .data
 align 0x1000
 init_pml4:
-	dq init_pdp1 - KERNEL_VMA + 3
-	times 255 dq 0
-	dq phys_pdp - KERNEL_VMA + 3
-	times 254 dq 0
-	dq init_pdp2 - KERNEL_VMA + 3
+    dq init_pdp1 - KERNEL_VMA + 3
+    times 255 dq 0
+    dq phys_pdp - KERNEL_VMA + 3
+    times 254 dq 0
+    dq init_pdp2 - KERNEL_VMA + 3
 
 %macro gen_pd_2mb 3
-	%assign i %1
-	%rep %2
-		dq (i | 0x83)
-		%assign i i + 0x200000
-	%endrep
-	%rep %3
-		dq 0
-	%endrep
+    %assign i %1
+    %rep %2
+        dq (i | 0x83)
+        %assign i i + 0x200000
+    %endrep
+    %rep %3
+        dq 0
+    %endrep
 %endmacro
 
 align 0x1000
 init_pdp1:
-	dq init_pd - KERNEL_VMA + 3
-	times 511 dq 0
+    dq init_pd - KERNEL_VMA + 3
+    times 511 dq 0
 
 align 0x1000
 init_pdp2:
-	times 510 dq 0
-	dq init_pd - KERNEL_VMA + 3
-	dq 0
+    times 510 dq 0
+    dq init_pd - KERNEL_VMA + 3
+    dq 0
 
 align 0x1000
 phys_pdp:
-	dq phys_pd - KERNEL_VMA + 3
-	times 511 dq 0
+    dq phys_pd - KERNEL_VMA + 3
+    times 511 dq 0
 
 align 0x1000
 init_pd:
-	gen_pd_2mb 0, 64, 448
+    gen_pd_2mb 0, 64, 448
 
 align 0x1000
 phys_pd:
-	gen_pd_2mb 0, 512, 0
+    gen_pd_2mb 0, 512, 0
 
 section .data
 
 align 0x10
 init_gdt:
-	dq 0
-	dq 0x00209A0000000000
+    dq 0
+    dq 0x00209A0000000000
 init_gdt_end:
 
 init_gdt_ptr:
     dw init_gdt_end - init_gdt - 1
-	dq init_gdt - KERNEL_VMA
+    dq init_gdt - KERNEL_VMA
 
 init_gdt_ptr_high:
-	dw init_gdt_end - init_gdt - 1
-	dq init_gdt
+    dw init_gdt_end - init_gdt - 1
+    dq init_gdt
 
 section .text
 
@@ -114,7 +114,7 @@ _loader:
     cli
     lgdt [init_gdt_ptr - KERNEL_VMA]
     mov esp, stack_end - KERNEL_VMA
-	call cpuid_available
+    call cpuid_available
     mov eax, 0x80000000
     cpuid
     cmp eax, 0x80000001
@@ -123,42 +123,42 @@ _loader:
     cpuid
     test edx, 1 << 29
     jz stop  
-	push 0
-	push eax
-	push 0
-	push ebx 
-	mov eax, cr4
-	or eax, 0x000000A0
-	mov cr4, eax
-	mov eax, init_pml4 - KERNEL_VMA
-	mov cr3, eax
-	mov ecx, 0xC0000080
-	rdmsr
-	or eax, 0x00000101
-	wrmsr
-	mov eax, cr0
-	or eax, 0x80000001
-	mov cr0, eax
-	jmp 0x08:higher_half_entry
+    push 0
+    push eax
+    push 0
+    push ebx 
+    mov eax, cr4
+    or eax, 0x000000A0
+    mov cr4, eax
+    mov eax, init_pml4 - KERNEL_VMA
+    mov cr3, eax
+    mov ecx, 0xC0000080
+    rdmsr
+    or eax, 0x00000101
+    wrmsr
+    mov eax, cr0
+    or eax, 0x80000001
+    mov cr0, eax
+    jmp 0x08:higher_half_entry
 
 bits 64
 
 higher_half_entry equ (_higher_half_entry - KERNEL_VMA)
 
 _higher_half_entry:
-	mov rax, _entry
-	jmp rax
+    mov rax, _entry
+    jmp rax
 
 _entry:
-	lgdt [init_gdt_ptr_high]
-	mov ax, 0x0
-	mov ds, ax
-	mov es, ax
-	mov ss, ax
-	mov gs, ax
-	mov fs, ax
-	add rsp, KERNEL_VMA
-	pop rdi
+    lgdt [init_gdt_ptr_high]
+    mov ax, 0x0
+    mov ds, ax
+    mov es, ax
+    mov ss, ax
+    mov gs, ax
+    mov fs, ax
+    add rsp, KERNEL_VMA
+    pop rdi
     call kernel_main
     hlt
 
